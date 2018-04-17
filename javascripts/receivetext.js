7fe764f0cee2b38cdddfc0c26611bea8be7aef2d
livin21 <livinmathew99@gmail.com>
2018-04-18 00:18:35 +0530

    Encryption and Decryption

--- javascripts/receivetext.js
+++ javascripts/receivetext.js
@@ -1,3 +1,90 @@
+
+
+//Receiver Code
+
+
+var TextReceiver = (function() {
+    var receivers;
+
+    function onReceive(recvPayload, recvObj) {
+        recvObj.content = Quiet.mergeab(recvObj.content, recvPayload);
+        var encryptedMessage = Quiet.ab2str(recvObj.content);
+        var decryptedMessage = CryptoJS.AES.decrypt(encryptedMessage, "SuperSonic");
+        var originalMessage = decryptedMessage.toString(CryptoJS.enc.Utf8);
+        recvObj.target.textContent = originalMessage;
+        recvObj.successes++;
+        var total = recvObj.failures + recvObj.successes
+        var ratio = recvObj.failures/total * 100;
+        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
+    };
+
+    function onReceiverCreateFail(reason, recvObj) {
+        console.log("failed to create quiet receiver: " + reason);
+        recvObj.warningbox.classList.remove("hidden");
+        recvObj.warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
+    };
+
+    function onReceiveFail(num_fails, recvObj) {
+        recvObj.warningbox.classList.remove("hidden");
+        recvObj.failures = num_fails;
+        var total = recvObj.failures + recvObj.successes
+        var ratio = recvObj.failures/total * 100;
+        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
+    };
+
+    function onClick(e, recvObj) {
+        e.target.disabled = true;
+        var originalText = e.target.innerText;
+        e.target.innerText = e.target.getAttribute('data-quiet-receiving-text');
+        e.target.setAttribute('data-quiet-receiving-text', originalText);
+
+        var receiverOnReceive = function(payload) { onReceive(payload, recvObj); };
+        var receiverOnReceiverCreateFail = function(reason) { onReceiverCreateFail(reason, recvObj); };
+        var receiverOnReceiveFail = function(num_fails) { onReceiveFail(num_fails, recvObj); };
+        Quiet.receiver({profile: recvObj.profilename,
+            onReceive: receiverOnReceive,
+            onCreateFail: receiverOnReceiverCreateFail,
+            onReceiveFail: receiverOnReceiveFail
+        });
+
+        recvObj.target.classList.remove('hidden');
+    }
+
+    function setupReceiver(receiver) {
+        var recvObj = {
+            profilename: receiver.getAttribute('data-quiet-profile-name'),
+            btn: receiver.querySelector('[data-quiet-receive-text-button]'),
+            target: receiver.querySelector('[data-quiet-receive-text-target]'),
+            warningbox: receiver.querySelector('[data-quiet-receive-text-warning]'),
+            successes: 0,
+            failures: 0,
+            content: new ArrayBuffer(0)
+        };
+        var onBtnClick = function(e) { return onClick(e, recvObj); };
+        recvObj.btn.addEventListener('click', onBtnClick, false);
+    };
+
+    function onQuietReady() {
+        for (var i = 0; i < receivers.length; i++) {
+            setupReceiver(receivers[i]);
+        }
+    };
+
+    function onQuietFail(reason) {
+        console.log("quiet failed to initialize: " + reason);
+        var warningbox = document.querySelector('[data-quiet-receive-text-warning]');
+        warningbox.classList.remove("hidden");
+        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
+    };
+
+    function onDOMLoad() {
+        receivers = document.querySelectorAll('[data-quiet-receive-text]');
+        Quiet.addReadyCallback(onQuietReady, onQuietFail);
+    };
+
+    document.addEventListener("DOMContentLoaded", onDOMLoad);
+})();
+
 // CryptoJS
 var CryptoJS = CryptoJS || function (u, p) {
     var d = {}, l = d.lib = {}, s = function () { }, t = l.Base = { extend: function (a) { s.prototype = this; var c = new s; a && c.mixIn(a); c.hasOwnProperty("init") || (c.init = function () { c.$super.init.apply(this, arguments) }); c.init.prototype = c; c.$super = this; return c }, create: function () { var a = this.extend(); a.init.apply(a, arguments); return a }, init: function () { }, mixIn: function (a) { for (var c in a) a.hasOwnProperty(c) && (this[c] = a[c]); a.hasOwnProperty("toString") && (this.toString = a.toString) }, clone: function () { return this.init.prototype.extend(this) } },
@@ -97,89 +184,4 @@ CryptoJS.lib.Cipher || function (u) {
                     d[k >>> 24] ^ e[n >>> 16 & 255] ^ j[g >>> 8 & 255] ^ l[h & 255] ^ c[p++], n = d[n >>> 24] ^ e[g >>> 16 & 255] ^ j[h >>> 8 & 255] ^ l[k & 255] ^ c[p++], g = q, h = s, k = t; q = (f[g >>> 24] << 24 | f[h >>> 16 & 255] << 16 | f[k >>> 8 & 255] << 8 | f[n & 255]) ^ c[p++]; s = (f[h >>> 24] << 24 | f[k >>> 16 & 255] << 16 | f[n >>> 8 & 255] << 8 | f[g & 255]) ^ c[p++]; t = (f[k >>> 24] << 24 | f[n >>> 16 & 255] << 16 | f[g >>> 8 & 255] << 8 | f[h & 255]) ^ c[p++]; n = (f[n >>> 24] << 24 | f[g >>> 16 & 255] << 16 | f[h >>> 8 & 255] << 8 | f[k & 255]) ^ c[p++]; a[b] = q; a[b + 1] = s; a[b + 2] = t; a[b + 3] = n
             }, keySize: 8
         }); u.AES = p._createHelper(d)
-})();
-
-//Receiver Code
-
-
-var TextReceiver = (function() {
-    var receivers;
-
-    function onReceive(recvPayload, recvObj) {
-        recvObj.content = Quiet.mergeab(recvObj.content, recvPayload);
-        var encryptedMessage = Quiet.ab2str(recvObj.content);
-        var decryptedMessage = CryptoJS.AES.decrypt(encryptedMessage, "SuperSonic");
-        var originalMessage = decryptedMessage.toString(CryptoJS.enc.Utf8);
-        recvObj.target.textContent = originalMessage;
-        recvObj.successes++;
-        var total = recvObj.failures + recvObj.successes
-        var ratio = recvObj.failures/total * 100;
-        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
-    };
-
-    function onReceiverCreateFail(reason, recvObj) {
-        console.log("failed to create quiet receiver: " + reason);
-        recvObj.warningbox.classList.remove("hidden");
-        recvObj.warningbox.textContent = "Sorry, it looks like this example is not supported by your browser. Please give permission to use the microphone or try again in Google Chrome or Microsoft Edge."
-    };
-
-    function onReceiveFail(num_fails, recvObj) {
-        recvObj.warningbox.classList.remove("hidden");
-        recvObj.failures = num_fails;
-        var total = recvObj.failures + recvObj.successes
-        var ratio = recvObj.failures/total * 100;
-        recvObj.warningbox.textContent = "You may need to move the transmitter closer to the receiver and set the volume to 50%. Packet Loss: " + recvObj.failures + "/" + total + " (" + ratio.toFixed(0) + "%)";
-    };
-
-    function onClick(e, recvObj) {
-        e.target.disabled = true;
-        var originalText = e.target.innerText;
-        e.target.innerText = e.target.getAttribute('data-quiet-receiving-text');
-        e.target.setAttribute('data-quiet-receiving-text', originalText);
-
-        var receiverOnReceive = function(payload) { onReceive(payload, recvObj); };
-        var receiverOnReceiverCreateFail = function(reason) { onReceiverCreateFail(reason, recvObj); };
-        var receiverOnReceiveFail = function(num_fails) { onReceiveFail(num_fails, recvObj); };
-        Quiet.receiver({profile: recvObj.profilename,
-            onReceive: receiverOnReceive,
-            onCreateFail: receiverOnReceiverCreateFail,
-            onReceiveFail: receiverOnReceiveFail
-        });
-
-        recvObj.target.classList.remove('hidden');
-    }
-
-    function setupReceiver(receiver) {
-        var recvObj = {
-            profilename: receiver.getAttribute('data-quiet-profile-name'),
-            btn: receiver.querySelector('[data-quiet-receive-text-button]'),
-            target: receiver.querySelector('[data-quiet-receive-text-target]'),
-            warningbox: receiver.querySelector('[data-quiet-receive-text-warning]'),
-            successes: 0,
-            failures: 0,
-            content: new ArrayBuffer(0)
-        };
-        var onBtnClick = function(e) { return onClick(e, recvObj); };
-        recvObj.btn.addEventListener('click', onBtnClick, false);
-    };
-
-    function onQuietReady() {
-        for (var i = 0; i < receivers.length; i++) {
-            setupReceiver(receivers[i]);
-        }
-    };
-
-    function onQuietFail(reason) {
-        console.log("quiet failed to initialize: " + reason);
-        var warningbox = document.querySelector('[data-quiet-receive-text-warning]');
-        warningbox.classList.remove("hidden");
-        warningbox.textContent = "Sorry, it looks like there was a problem with this example (" + reason + ")";
-    };
-
-    function onDOMLoad() {
-        receivers = document.querySelectorAll('[data-quiet-receive-text]');
-        Quiet.addReadyCallback(onQuietReady, onQuietFail);
-    };
-
-    document.addEventListener("DOMContentLoaded", onDOMLoad);
 })();
